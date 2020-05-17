efe88a0c3c983f2b09c2a93788670d54bcc14b81
Erich Schneider <erich.schneider97@protonmail.com>
2020-05-17 02:28:30 +0200

    Add more stun servers

--- public/index.js
+++ public/index.js
@@ -296,7 +296,16 @@ $(".add-file").addEventListener("click", ({ target: btn }) => {
   file_input.click();
 });
 
-const rtcConfig = { iceServers: [{ urls: "stun://stun.l.google.com:19302" }] };
+const rtcConfig = {
+  iceServers: [
+    { urls: "stun://stun.l.google.com:19302" },
+    { urls: "stun://stun1.l.google.com:19302" },
+    { urls: "stun://stun2.l.google.com:19302" },
+    { urls: "stun://stun3.l.google.com:19302" },
+    { urls: "stun://stun4.l.google.com:19302" },
+    { urls: "stun.ekiga.net" },
+  ],
+};
 
 const ws = new WebSocket(`wss://${location.host}`, "json");
 
